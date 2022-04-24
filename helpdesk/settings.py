c6bb9a105f883ee2088bc03f8ba12218eaa86196
siva04072001 <87768280+siva04072001@users.noreply.github.com>
2022-04-24 06:20:08 +0530

    Add files via upload

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -38,18 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tickets.apps.TicketsConfig',
-    
+    'tickets',    
 ]
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-
 
 AUTH_USER_MODEL='tickets.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,10 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -143,3 +136,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "svshelpdesk2001@gmail.com"
 EMAIL_HOST_PASSWORD = "svs@2001"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
